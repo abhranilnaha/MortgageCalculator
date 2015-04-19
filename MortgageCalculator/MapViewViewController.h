3fb789fa21e2fd7405179d2b3d856a54df4754ba
Abhranil Naha <abhranilnaha@gmail.com>
2015-04-18 19:13:42 -0700

    Adding project for Mortgage Calculator

--- MortgageCalculator/MapViewViewController.h
+++ MortgageCalculator/MapViewViewController.h
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.h
+//  MapViewViewController.h
 //  MortgageCalculator
 //
 //  Created by loaner on 4/16/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SecondViewController : UIViewController
-
+@interface MapViewViewController : UIViewController
 
 @end
-
---
+++ MortgageCalculator/MortgageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MortgageViewController.h
+//  MortgageCalculator
+//
+//  Created by loaner on 4/18/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MortgageViewController : UIViewController
+
+@end
